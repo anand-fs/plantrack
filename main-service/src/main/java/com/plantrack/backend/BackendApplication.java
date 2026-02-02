b03faf81eaf44dbc381910ad9d5667e9d9d6577d
anand-fs <Anand.SP@cognizant.com>
2026-02-02 10:51:41 +0530

    created basic spring boot project for main microservice

---
+++ main-service/src/main/java/com/plantrack/backend/BackendApplication.java
@@ -0,0 +1,18 @@
+package com.plantrack.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@EnableJpaAuditing // <--- Enables automatic dates (CreatedDate)
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
