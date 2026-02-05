97bec001ae02687e7710ae1986874a4c063eb7b7
anand-fs <Anand.SP@cognizant.com>
2026-02-05 09:54:05 +0530

    Enabled use feign clients and added a bat script to run all microservices

--- main-service/src/main/java/com/plantrack/backend/BackendApplication.java
+++ main-service/src/main/java/com/plantrack/backend/BackendApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableJpaAuditing // <--- Enables automatic dates (CreatedDate)
+@EnableFeignClients
 public class BackendApplication {
 
 	public static void main(String[] args) {
---
+++ start_all.bat
@@ -0,0 +1,4 @@
+start cmd /k "cd plantrack-eureka && mvn spring-boot:run"
+start cmd /k "cd main-service && mvn spring-boot:run"
+start cmd /k "cd notification-service && mvn spring-boot:run"
+start cmd /k "cd plantrack-gateway && mvn spring-boot:run"
